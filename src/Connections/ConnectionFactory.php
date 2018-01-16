20b3e10dd5d841d2596cf29dd284f8b56ce0ba68
Rohit Sodhia <rohit@rhovisions.com>
2018-01-15 23:15:41 -0500

    Add docblocks and use statements to ConnectionFactory

--- src/Connections/ConnectionFactory.php
+++ src/Connections/ConnectionFactory.php
@@ -2,55 +2,98 @@
 
 namespace phpDM\Connections;
 
+use phpDM\QueryBuilder\{
+	MongoQueryBuilder, MysqlQueryBuilder, QueryBuilder
+};
+use phpDM\Models\{
+	MongoModel,
+	MysqlModel
+};
+
+/**
+ * Class ConnectionFactory
+ * @package phpDM\Connections
+ */
 class ConnectionFactory
 {
 
+	/**
+	 * @var array List of added interfaces
+	 */
 	private static $connectionInterfaces = [];
+
+	/**
+	 * @var array List of added query builders
+	 */
 	private static $queryBuilders = [];
 
+	/**
+	 * Registers connection details depending on installed packages
+	 */
 	public static function init() {
 		if (class_exists('\MongoClient')) {
 			self::registerConnection('mongo', [
 				'interface' => Interfaces\MongoConnectionInterface::class,
-				'queryBuilder' => \phpDM\QueryBuilder\MongoQueryBuilder::class,
-				'model' => \phpDM\Models\MongoModel::class
+				'queryBuilder' => MongoQueryBuilder::class,
+				'model' => MongoModel::class
 			]);
 		}
 		if (class_exists('\PDO')) {
 			self::registerConnection('mysql', [
 				'interface' => Interfaces\MysqlConnectionInterface::class,
-				'queryBuilder' => \phpDM\QueryBuilder\MysqlQueryBuilder::class,
-				'model' => \phpDM\Models\MysqlModel::class
+				'queryBuilder' => MysqlQueryBuilder::class,
+				'model' => MysqlModel::class
 			]);
 		}
 	}
 
+	/**
+	 * Registers a connection, 
+	 * @param string $type
+	 * @param array $config
+	 */
 	public static function registerConnection(string $type, array $config) {
 		self::$connectionInterfaces[$type] = $config['interface'];
 		self::$queryBuilders[$type] = $config['queryBuilder'];
 	}
 
+	/**
+	 * @return array
+	 */
 	public static function getConnectionInterfaces(): array {
 		return self::$connectionInterfaces;
 	}
 
-	public static function getConnectionInterface(string $type) {
+	/**
+	 * @param string $type
+	 * @return string
+	 * @throws \Exception
+	 */
+	public static function getConnectionInterface(string $type): string {
 		if (isset(self::$connectionInterfaces[$type])) {
 			return self::$connectionInterfaces[$type];
 		} else {
-			throw Exception('Invalid connection');
+			throw new \Exception('Invalid connection');
 		}
 	}
 
+	/**
+	 * @return array
+	 */
 	public static function getQueryBuilders(): array {
 		return self::$queryBuilders;
 	}
 
-	public static function getQueryBuilder(string $type) {
+	/**
+	 * @param string $type Query Builder type
+	 * @return QueryBuilder
+	 * @throws \Exception
+	 */
+	public static function getQueryBuilder(string $type): QueryBuilder {
 		if (isset(self::$queryBuilders[$type])) {
 			return self::$queryBuilders[$type];
 		} else {
-			throw Exception('Invalid query builder');
+			throw new \Exception('Invalid query builder');
 		}
 	}
 
